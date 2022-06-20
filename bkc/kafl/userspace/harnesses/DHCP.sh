3a7ba2ed55d5389630693d2b0edd592a9992f3f6
Sebastian Österlund <sebastian.osterlund@intel.com>
2022-06-20 07:49:39 -0700

    run_experiments.py: add support for US_ harnesses
    userspace harnesses run the NONE kernel harness, and copy over a
    userspace harness shell script from bkc/kafl/userspace/harnesses.
    
    For example, the US_DHCP harness, would copy over
    bkc/kafl/userspace/harnesses/DHCP.sh into sharedir/init.sh, so that the
    harness is run as a userspace payload.

---
+++ bkc/kafl/userspace/harnesses/DHCP.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# 
+# Copyright (C)  2022  Intel Corporation. 
+#
+# This software and the related documents are Intel copyrighted materials, and your use of them is governed by the express license under which they were provided to you ("License"). Unless the License provides otherwise, you may not use, modify, copy, publish, distribute, disclose or transmit this software or the related documents without Intel's prior written permission.
+# This software and the related documents are provided as is, with no express or implied warranties, other than those that are expressly stated in the License.
+#
+# SPDX-License-Identifier: MIT
+
+mount -t debugfs none /sys/kernel/debug/
+KAFL_CTL=/sys/kernel/debug/kafl
+
+echo "[*] kAFL userspace harness DHCP. Agent status:"
+grep . $KAFL_CTL/*
+grep . $KAFL_CTL/status/*
+
+## BEGIN HARNESS
+
+echo "enable"  > $KAFL_CTL/control
+udhcpc 2>&1 |hcat
+echo "done"  > $KAFL_CTL/control
+
+## END HARNESS
